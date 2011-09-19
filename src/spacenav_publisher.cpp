d296b0b6a27aafcad45117e18c1346875f25f4fe
Tim Clephas <t.t.g.clephas@alumnus.tue.nl>
2011-09-19 12:14:29 +0000

    dev2trunk
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_teleop/amigo_teleop_spacenav@5667 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- src/spacenav_publisher.cpp
+++ src/spacenav_publisher.cpp
@@ -3,134 +3,218 @@
 #include <math.h>
 
 #include <geometry_msgs/Twist.h>
+#include <amigo_msgs/head_ref.h>
+#include <amigo_kinematics/set_Arm.h>
+
+// Defined variables: //@TODO: Create parameters
+#define minumum_movement 0.2
+#define joystick_range 0.68
+#define max_translational_speed 0.5
+#define max_angular_speed 0.8
 
 
-double x_min=0.3;
-double y_min=0.3;
-double z_min=0.3;
-
-double x_max=0.5;
-double y_max=0.5;
-double z_max=0.5;
-double z_ang_max=0.8;
-
-
+// Global variables
+bool pressed;
 double init_time;
-double current_time;
-
-double x_range=0.68;
-double y_range=0.68;
-double z_range=0.68;
-double z_ang_range=0.68;
+double last_time;
+uint mode = 0;
 
 ros::Publisher vel_pub;
-geometry_msgs::Twist vel_msg;
-int mode=1;
-double timer=0;
-double check=0.1;
-double linear_x;
-double linear_y;
-double linear_z;
-double angular_z;
+ros::Publisher head_pub;
+ros::ServiceClient arm_left_pub;
+ros::ServiceClient arm_right_pub;
 
+// Arms:
+double lin_scale = 0.05;
+double ang_scale = 0.1;
+
+double prev_left_pos[6] = { 0.4, 0.2, 0.8, 0.0, 0.0, 0.0 };
+double prev_right_pos[6] = { 0.4, -0.2, 0.8, 0.0, 0.0, 0.0 };
 
 
 
 void SpaceNavCallback(const joy::Joy::ConstPtr& msg)
-{  current_time = ros::WallTime::now().toSec()-init_time;
-   
-   int button=msg->buttons[0];
-      check=ros::WallTime::now().toSec()-init_time-timer;
-   if (button==1){
+{
+	double current_time = ros::WallTime::now().toSec() - init_time;
+	double dt = current_time - last_time;
+	last_time = current_time;
 
-      if (mode==1 && check>=0.5){
-          mode=2;
-          timer=ros::WallTime::now().toSec()-init_time;
-      }
-      else if (mode==2 && check>=0.5){
-          mode=1;
-          timer=ros::WallTime::now().toSec()-init_time;
-      }
-   };
+	bool button1 = msg->buttons[0];
+	bool button2 = msg->buttons[1];
 
-   
-   // moving base mode
-   if (mode==2){
-   ROS_INFO("Moving base mode");
-
-  linear_x=msg->axes[0]/x_range;
-   if (linear_x>x_min){
-      linear_x=(linear_x-x_min)/(1-x_min);
-      }
-   else if (linear_x<-x_min){ 
-     linear_x=(linear_x+x_min)/(1-x_min);
-     } 
-   else
-   {linear_x=0;}
+	// Normalise joystick commands:
+	double dof[6];
+	for ( uint i = 0; i < 6; i++ )
+	{
+		dof[i] = msg->axes[i] / joystick_range;
+		if ( dof[i] > minumum_movement )
+			dof[i] = ( dof[i] - minumum_movement )/( 1 - minumum_movement );
+		else if ( dof[i] < -minumum_movement )
+			dof[i] = ( dof[i] + minumum_movement )/( 1 - minumum_movement );
+		else
+			dof[i] = 0;
+	}
+	ROS_DEBUG("x = %f  \t  y = %f  \t  z = %f  \t  x_ang = %f  \t  y_ang = %f  \t  z_ang = %f  \t  ", dof[0], dof[1], dof[2], dof[3], dof[4], dof[5]);
 
 
-  linear_y=msg->axes[1]/y_range;
-   if (linear_y>y_min){
-      linear_y=(linear_y-y_min)/(1-y_min);
-      }
-   else if (linear_y<-y_min){ 
-     linear_y=(linear_y+y_min)/(1-y_min);
-     } 
-   else
-   {linear_y=0;}
+	// Switch between modes:
+	if (button1){
+		if (mode <= 2 && !pressed){
+			mode++;
+			ROS_INFO("Mode = %u",mode);
+		}
+		else if (!pressed)
+		{
+			mode = 0;
+			ROS_INFO("Mode = %u",mode);
+		}
+		pressed = true;
+	}
+	else
+		pressed = false;
 
- 
-  linear_z=msg->axes[2]/z_range;
-   if (linear_z>z_min){
-      linear_z=(linear_z-z_min)/(1-z_min);
-      }
-   else if (linear_z<-z_min){ 
-     linear_z=(linear_z+z_min)/(1-z_min);
-     } 
-   else
-   {linear_z=0;}
+	// Switch between operating modes:
+	switch(mode)
+	{
+	case 1:
+	{
+		ROS_INFO("Moving head mode");
+		double pan = dof[5];
+		double tilt = dof[4];
 
+		double angle_factor = 1;
+		if (button2)
+		{
+			ROS_INFO("Bigger angle");
+			angle_factor = 2.5;
+		}
+		amigo_msgs::head_ref head_msg;
+		head_msg.head_pan = pan*angle_factor;
+		head_msg.head_tilt = tilt;
+		head_pub.publish(head_msg);
+		ROS_DEBUG("pan = %f \t tilt = %f \t ",head_msg.head_pan,head_msg.head_tilt);
+		break;
+	}
+	case 2:
+	{
+		ROS_INFO("Moving base mode");
 
-   angular_z=msg->axes[5]/z_ang_range;
-   if (angular_z>z_min){
-      angular_z=(angular_z-z_min)/(1-z_min);
-      }
-   else if (angular_z<-z_min){ 
-     angular_z=(angular_z+z_min)/(1-z_min);
-     } 
-   else
-   {angular_z=0;}
+		geometry_msgs::Twist vel_msg;
+		vel_msg.linear.x = max_translational_speed * dof[0];
+		vel_msg.linear.y = max_translational_speed * dof[1];
+		vel_msg.linear.z = max_translational_speed * dof[2];
+		vel_msg.angular.z = max_angular_speed * dof[5];
 
+		vel_pub.publish(vel_msg);
+		break;
+	}
+	case 3:
+	{
+		ROS_INFO("Moving left arm mode");
 
-   vel_msg.linear.x=x_max*linear_x;
-   vel_msg.linear.y=y_max*linear_y;
-   vel_msg.linear.z=z_max*linear_z;
-   vel_msg.angular.z=z_ang_max*angular_z;
+		//numerically integrate
+		double pos[6];
+		for ( uint i = 0; i < 3; i++ )
+			pos[i] = prev_left_pos[i] + lin_scale * dof[i] * dt;
+		for ( uint i = 3; i < 6; i++ )
+			pos[i] = prev_left_pos[i] + ang_scale * dof[i] * dt;
 
-   vel_pub.publish(vel_msg);
-   ROS_INFO("x=%f\ty=%f\tz=%f\tz_ang=%f\t",vel_msg.linear.x,vel_msg.linear.y,vel_msg.linear.z,vel_msg.angular.z);
-   };
+		ROS_INFO("x = %f  \t y = %f \t z = %f \t r = %f \t p = %f \t y = %f \t dt = %f \t ", pos[0], pos[1], pos[2], pos[3], pos[4], pos[5], dt );
 
+		amigo_kinematics::set_Arm arm_srv;
+		arm_srv.request.x = pos[0];
+		arm_srv.request.y = pos[1];
+		arm_srv.request.z = pos[2];
+		arm_srv.request.roll = pos[3];
+		arm_srv.request.pitch = pos[4];
+		arm_srv.request.yaw = pos[5];
 
-   
+		bool change = false;
+		for ( uint i = 0; i < 3; i++ )
+			if (pos[i] != prev_left_pos[i])
+				change = true;
+
+		if (change)
+		{
+			if (arm_left_pub.call(arm_srv))
+			{
+				if (arm_srv.response.ik_valid)
+				{
+					ROS_INFO("Jeej");
+					for ( uint i = 0; i < 6; i++ )
+						prev_left_pos[i] = pos[i];
+				}
+				else
+				{
+					ROS_INFO("Invalid position.");
+				}
+			}
+			else
+			{
+				ROS_ERROR("Failed to call service set_Arm");
+			}
+		}
+		break;
+	}
+	case 4:
+	{
+		ROS_INFO("Moving right arm mode");
+
+		//numerically integrate
+		double pos[6];
+		for ( uint i = 0; i < 6; i++ )
+			pos[i] = prev_right_pos[i] + dof[i] * dt;
+
+		ROS_INFO("x = %f  \t y = %f \t z = %f \t r = %f \t p = %f \t y = %f \t dt = %f \t ", pos[0], pos[1], pos[2], pos[3], pos[4], pos[5], dt );
+
+		amigo_kinematics::set_Arm arm_srv;
+		arm_srv.request.x = pos[0];
+		arm_srv.request.y = pos[1];
+		arm_srv.request.z = pos[2];
+		arm_srv.request.roll = -pos[3];
+		arm_srv.request.pitch = -pos[4];
+		arm_srv.request.yaw = -pos[5];
+
+		if (arm_right_pub.call(arm_srv))
+		{
+			if (arm_srv.response.ik_valid)
+			{
+				ROS_INFO("Jeej");
+				for ( uint i = 0; i < 6; i++ )
+					prev_right_pos[i] = pos[i];
+			}
+			else
+			{
+				ROS_INFO("Invalid position.");
+			}
+		}
+		else
+		{
+			ROS_ERROR("Failed to call service set_Arm");
+		}
+		break;
+	}
+	}
+
 }
 
-
-
 int main(int argc, char** argv){
-  ros::init(argc, argv, "spacenav_publisher");
+	ros::init(argc, argv, "spacenav_publisher");
 
-  ros::NodeHandle n;
-  ros::Subscriber spacenav_sub = n.subscribe("/spacenav/joy", 1, &SpaceNavCallback);
-  vel_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
-  ros::Rate rate(1000);
-  ros::WallTime current_time2;
-  init_time = ros::WallTime::now().toSec();
-  while (n.ok()){
+	ros::NodeHandle n;
+	ros::Subscriber spacenav_sub = n.subscribe("/spacenav/joy", 1, &SpaceNavCallback);
+	vel_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
+	head_pub = n.advertise<amigo_msgs::head_ref>("/head_controller/set_Head", 1);
+	arm_left_pub = n.serviceClient<amigo_kinematics::set_Arm>("/arm_left/set_tip");
+	arm_right_pub = n.serviceClient<amigo_kinematics::set_Arm>("/arm_right/set_tip");
+	init_time = ros::WallTime::now().toSec();
 
-  ros::spinOnce();
-  rate.sleep();
-  }
-  return 1;
+	ros::Rate rate(15);
+	while (n.ok()){
+
+		ros::spinOnce();
+		rate.sleep();
+	}
+	return 1;
 
 }
